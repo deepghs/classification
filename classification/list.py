ccccda35106a53b3048f98d8246bf1d74ff45f3c
narugo1992 <narugo992@gmail.com>
2024-03-18 15:52:03 +0800

    dev(narugo): up

--- classification/list.py
+++ classification/list.py
@@ -45,8 +45,6 @@ HUGGINGFACE_CO_PAGE_TEMPLATE = ENDPOINT + "/{repo_id}/blob/{revision}/{filename}
 
 @cli.command('huggingface', context_settings={**GLOBAL_CONTEXT_SETTINGS},
              help='Publish model to huggingface model repository')
-@click.option('--imgsize', '-s', 'imgsize', type=int, default=384,
-              help='Image size for input.', show_default=True)
 @click.option('--repository', '-r', 'repository', type=str, required=True,
               help='Repository for publishing model.', show_default=True)
 @click.option('--revision', '-R', 'revision', type=str, default='main',
@@ -55,7 +53,7 @@ HUGGINGFACE_CO_PAGE_TEMPLATE = ENDPOINT + "/{repo_id}/blob/{revision}/{filename}
               help='Columns to be listed.')
 @click.option('--image', '-I', 'show_image', is_flag=True, type=bool, default=False,
               help='Show image in table.', show_default=True)
-def huggingface(imgsize: int, repository: str, revision: str, columns: Tuple[str, ...], show_image: bool):
+def huggingface(repository: str, revision: str, columns: Tuple[str, ...], show_image: bool):
     logging.try_init_root(logging.INFO)
     columns = columns or ('name', 'FLOPS', 'params', 'accuracy', 'AUC', 'confusion', 'labels')
 
@@ -68,6 +66,8 @@ def huggingface(imgsize: int, repository: str, revision: str, columns: Tuple[str
     rows = []
     for name in tqdm(names):
         files = hf_fs.ls(f'{repository}@{revision}/{name}')
+        meta_info = json.loads(hf_fs.read_text(f'{repository}@{revision}/{name}/meta.json'))
+        imgsize = meta_info.get('img_size', 384)
         values = {'name': name}
 
         model = load_model_from_ckpt(hf_client.hf_hub_download(repository, f'{name}/model.ckpt', revision=revision))
